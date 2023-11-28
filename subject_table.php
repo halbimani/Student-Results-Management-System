cd564a8b78a83cc03bf8d1932c273548e2d1c892
halbimani <54641264+halbimani@users.noreply.github.com>
2023-11-28 11:42:47 +0400

    Create subject_table.php

---
+++ subject_table.php
@@ -0,0 +1,69 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "student_grades";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn -> connect_errno)
+{
+    echo "Connection failed: " . $conn -> connect_error;
+    exit();
+}
+
+if (isset($_GET['subjectid']) && !empty($_GET['subjectid'])) {
+    $subjectid = $_GET['subjectid'];
+    $query = "SELECT subjectid, subjectname FROM subjects WHERE subjectid = " . $subjectid . ";";
+    $result = $conn->query($query);
+} else {
+    $sql = "select * from subjects";
+    $result = ($conn->query($sql));
+}
+
+$row = [];
+
+if ($result->num_rows > 0)
+{
+    $row = $result->fetch_all(MYSQLI_ASSOC);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Subjects</title>
+        <link rel="stylesheet" href="table_designCSS.css">
+    </head>
+    <body style="background-color: gray;">
+        <div class="box">
+        <h2 style="color: white;">Subjects Table</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Subject ID</th>
+                        <th>Subject Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if(!empty($row))
+                    foreach($row as $rows)
+                    {
+                    ?>
+                    <tr>
+                        <td><a href="grade_table_subject.php?subjectid=<?php echo $rows['subjectid'] ?>" style="color: white;">
+                            <?php echo $rows['subjectid']?>
+                        </a></td>
+                        <td><?php echo $rows['subjectname']?></td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
+
+<?php
+$conn->close();
+?>
