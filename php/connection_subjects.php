e23aea0ace1633ef0aab17afa78313acf99e5d19
halbimani <54641264+halbimani@users.noreply.github.com>
2023-12-12 22:21:19 +0400

    Update connection_subjects.php

--- php/connection_subjects.php
+++ php/connection_subjects.php
@@ -1,14 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "student_grades";
-
-$conn = new mysqli($servername, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'server_config.php';
 
 $subjectid = $_POST['subjectid'];
 $subjectname = $_POST['subjectname'];
