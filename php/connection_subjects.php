f2ae609aa8d37106a4281a2e057920f85ec003c3
halbimani <54641264+halbimani@users.noreply.github.com>
2023-12-14 06:21:12 +0400

    Update connection_subjects.php

--- php/connection_subjects.php
+++ php/connection_subjects.php
@@ -1,10 +1,9 @@
 <?php
 include 'server_config.php';
 
-$subjectid = $_POST['subjectid'];
 $subjectname = $_POST['subjectname'];
 
-$sql = "INSERT INTO subjects (subjectid, subjectname) VALUES ('$subjectid', '$subjectname')";
+$sql = "INSERT INTO subjects (subjectname) VALUES ('$subjectname')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Data inserted successfully.";
